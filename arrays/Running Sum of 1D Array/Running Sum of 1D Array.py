1a767aca0136414e904b22383a8a026473ba272b
nivedita bhagat <123383795+NiveditaBhagat@users.noreply.github.com>
2026-06-07 14:14:01 +0530

    Update Running Sum of 1D Array.py

--- arrays/Running Sum of 1D Array/Running Sum of 1D Array.py
+++ arrays/Running Sum of 1D Array/Running Sum of 1D Array.py
@@ -4,3 +4,18 @@ class Solution(object):
         for i in range(1, len(nums)):
             nums[i] = nums[i] + nums[i - 1]
         return nums
+
+
+
+# Approach 2: Using a New Array (Beginner Friendly)
+
+class Solution(object):
+    def runningSum(self, nums):
+        result = []
+        curr_sum = 0
+
+        for num in nums:
+            curr_sum += num
+            result.append(curr_sum)
+
+        return result
