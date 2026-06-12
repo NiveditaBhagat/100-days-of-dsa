2dad3e047dfa1b604445b7f78a0ec2ac2ea53fbf
nivedita bhagat <123383795+NiveditaBhagat@users.noreply.github.com>
2026-06-12 12:08:30 +0530

    Update Group Anagrams.py

--- HashMap/Group Anagrams/Group Anagrams.py
+++ HashMap/Group Anagrams/Group Anagrams.py
@@ -1,4 +1,4 @@
-🧾 Code (Sorting Based)
+##🧾 Code (Sorting Based)
 from collections import defaultdict
 
 class Solution(object):
@@ -8,5 +8,23 @@ class Solution(object):
         for word in strs:
             key = ''.join(sorted(word))
             groups[key].append(word)
+
+        return list(groups.values())
+
+
+
+##🧾 Code (Frequency Based)
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+
+            groups[tuple(count)].append(word)
 
         return list(groups.values())
