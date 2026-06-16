4bedf3f7953f0ac8d498b6b46725aa22958eaac1
nivedita bhagat <123383795+NiveditaBhagat@users.noreply.github.com>
2026-06-16 18:44:17 +0530

    Create Merge Strings Alternately.py

---
+++ arrays/Merge Strings Alternately/Merge Strings Alternately.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        result=""
+        max_len=max(len(word1),len(word2))
+        for i in range(max_len):
+            if i<len(word1):
+                result+=word1[i]
+            if i<len(word2):
+                result+=word2[i]
+        
+        return result
